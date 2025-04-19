23b137df8393f4c2f7b9f9e1893d453c4f070cab
Bro Buddy <sirstone@gmx.de>
2025-04-19 17:37:51 +0200

    changed navbar

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -3,20 +3,16 @@ import { Link } from 'react-router-dom'
 const MobileNav = () => {
     return (
         <section className="mobile-nav">
-            <Link to="/">
-                <span className="text-xs">Booklet</span>
-            </Link>
-
-            <Link to="/M001">
-                <span className="text-xs">Mr. Johnson</span>
+            <Link to="/C001">
+                <span className="text-xs">Character</span>
             </Link>
 
             <Link to="/T001">
                 <span className="text-xs">NeonHollow</span>
             </Link>
 
-            <Link to="/C001">
-                <span className="text-xs">Character</span>
+            <Link to="/M001">
+                <span className="text-xs">Mr. Johnson</span>
             </Link>
         </section>
     )
