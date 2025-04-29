af780071d92e335f7ddf77c0a5d9135ff94eb2d6
Bro Buddy <sirstone@gmx.de>
2025-04-29 17:37:15 +0200

    missed some

--- src/feature/city/components/ActionsView.tsx
+++ src/feature/city/components/ActionsView.tsx
@@ -1,6 +1,7 @@
 import Card from '@/components/Card'
 import { Link, useParams } from 'react-router-dom'
 import { ActionStep, FacilityAction } from '../FacilityType'
+import FadeIn from '@/components/FadeIn'
 
 type FacilityActionsProps = {
     actionList: FacilityAction[]
@@ -12,41 +13,49 @@ const ActionsView = ({ actionList }: FacilityActionsProps) => {
 
     return (
         <Card dice={inMegaCorp ? undefined : 2}>
-            {actionList.map((item: FacilityAction, actionIndex: number) => (
-                <div key={actionIndex}>
-                    <p>
-                        <strong className="highlight">{item.title}</strong>
-                    </p>
-                    <ul className="list-margin">
-                        {item.steps.map(
-                            (step: ActionStep, stepIndex: number) => {
-                                const [firstWord, ...remainingWords] =
-                                    step.text.split(' ')
+            <FadeIn>
+                {actionList.map((item: FacilityAction, actionIndex: number) => (
+                    <div key={actionIndex}>
+                        <p>
+                            <strong className="highlight">{item.title}</strong>
+                        </p>
+                        <ul className="list-margin">
+                            {item.steps.map(
+                                (step: ActionStep, stepIndex: number) => {
+                                    const [firstWord, ...remainingWords] =
+                                        step.text.split(' ')
 
-                                return (
-                                    <li key={stepIndex}>
-                                        {step.link ? (
-                                            <>
-                                                <strong>{firstWord}:</strong>{' '}
-                                                <Link
-                                                    to={`/resource${step.link}`}
-                                                >
+                                    return (
+                                        <li key={stepIndex}>
+                                            {step.link ? (
+                                                <>
+                                                    <strong>
+                                                        {firstWord}:
+                                                    </strong>{' '}
+                                                    <Link
+                                                        to={`/resource${step.link}`}
+                                                    >
+                                                        {remainingWords.join(
+                                                            ' '
+                                                        )}
+                                                    </Link>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <strong>
+                                                        {firstWord}:
+                                                    </strong>{' '}
                                                     {remainingWords.join(' ')}
-                                                </Link>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <strong>{firstWord}:</strong>{' '}
-                                                {remainingWords.join(' ')}
-                                            </>
-                                        )}
-                                    </li>
-                                )
-                            }
-                        )}
-                    </ul>
-                </div>
-            ))}
+                                                </>
+                                            )}
+                                        </li>
+                                    )
+                                }
+                            )}
+                        </ul>
+                    </div>
+                ))}
+            </FadeIn>
         </Card>
     )
 }
--- src/feature/city/components/RollsView.tsx
+++ src/feature/city/components/RollsView.tsx
@@ -1,6 +1,7 @@
 import Card from '@/components/Card'
 import { Link } from 'react-router-dom'
 import { FacilityRoll } from '../FacilityType'
+import FadeIn from '@/components/FadeIn'
 
 type FacilityRollsProps = {
     rollList: FacilityRoll[]
@@ -9,23 +10,27 @@ type FacilityRollsProps = {
 const RollsView = ({ rollList }: FacilityRollsProps) => {
     return (
         <Card dice={2}>
-            <p>
-                <strong className="highlight">
-                    Enter &#8594; -1 Energy &#8594; Roll 2d6:
-                </strong>
-            </p>
-            <ul className="list-margin">
-                {rollList.map((item: FacilityRoll, index: number) => (
-                    <li key={index}>
-                        <strong>Roll {item.roll}</strong> &#8594;{' '}
-                        {item.link ? (
-                            <Link to={`/event${item.link}`}>{item.text}</Link>
-                        ) : (
-                            <>{item.text}</>
-                        )}
-                    </li>
-                ))}
-            </ul>
+            <FadeIn>
+                <p>
+                    <strong className="highlight">
+                        Enter &#8594; -1 Energy &#8594; Roll 2d6:
+                    </strong>
+                </p>
+                <ul className="list-margin">
+                    {rollList.map((item: FacilityRoll, index: number) => (
+                        <li key={index}>
+                            <strong>Roll {item.roll}</strong> &#8594;{' '}
+                            {item.link ? (
+                                <Link to={`/event${item.link}`}>
+                                    {item.text}
+                                </Link>
+                            ) : (
+                                <>{item.text}</>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </FadeIn>
         </Card>
     )
 }
