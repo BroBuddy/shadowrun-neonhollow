7d4381704e94a8b2d6abeac45e10888649591126
Bro Buddy <sirstone@gmx.de>
2025-04-29 09:23:34 +0200

    added matrix into chara

--- src/feature/background/components/BackgroundView.tsx
+++ src/feature/background/components/BackgroundView.tsx
@@ -1,8 +1,5 @@
 import Card from '@/components/Card'
-import { Link } from 'react-router-dom'
-
-const rows = 6
-const cols = 6
+import MatrixTable from '@/components/MatrixTable'
 
 function BackgroundView() {
     return (
@@ -11,40 +8,7 @@ function BackgroundView() {
                 <strong className="highlight">Background:</strong>
             </p>
             <p>Your archetype is the tens digit - 🎲 Roll 1d6:</p>
-            <table>
-                <thead>
-                    <tr>
-                        <th>2d6</th>
-                        <th>1</th>
-                        <th>2</th>
-                        <th>3</th>
-                        <th>4</th>
-                        <th>5</th>
-                        <th>6</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Array.from({ length: rows }, (_, rowIndex: number) => (
-                        <tr key={`row-${rowIndex + 1}`}>
-                            <th>{rowIndex + 1}</th>
-                            {Array.from(
-                                { length: cols },
-                                (_, colIndex: number) => (
-                                    <td
-                                        key={`cell-${rowIndex + 1}-${colIndex + 1}`}
-                                    >
-                                        <Link
-                                            to={`/background/R7${rowIndex + 1}${colIndex + 1}`}
-                                        >
-                                            {rowIndex + 1}x{colIndex + 1}
-                                        </Link>
-                                    </td>
-                                )
-                            )}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <MatrixTable linkPrefix="/background/R7" />
         </Card>
     )
 }
