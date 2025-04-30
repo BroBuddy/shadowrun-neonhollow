17a988d51086d927a313e33c335c80d168659672
Bro Buddy <sirstone@gmx.de>
2025-04-30 10:38:45 +0200

    added error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,5 +1,27 @@
+import Card from '@/components/Card'
+import Headline from '@/components/Headline'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 function Error() {
-    return <div>Error</div>
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            navigate('/city/safehouse')
+        }, 1000)
+
+        return () => clearTimeout(timeout)
+    }, [navigate])
+
+    return (
+        <>
+            <Headline>Critial Glitch</Headline>
+            <Card>
+                <p>You will be sent to your SafeHouse automatically.</p>
+            </Card>
+        </>
+    )
 }
 
 export default Error
