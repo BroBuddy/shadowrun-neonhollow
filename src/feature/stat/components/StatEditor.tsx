c9ed68dbcbb603a75df94640c66d3d7118481aa1
Bro Buddy <sirstone@gmx.de>
2025-05-03 15:37:23 +0200

    changed stat editor

--- src/feature/stat/components/StatEditor.tsx
+++ src/feature/stat/components/StatEditor.tsx
@@ -4,31 +4,57 @@ import StatRow from './StatRow'
 const StatEditor = () => {
     const stats = useStatStore((state) => state.stats)
     const modifyStats = useStatStore((state) => state.modifyStats)
+    const statEntries = Object.entries(stats)
+    const leftStats = statEntries.slice(0, 3)
+    const rightStats = statEntries.slice(3, 6)
 
     const handleModify = (stat: string, value: number) => {
         modifyStats({ [stat]: value })
     }
 
     return (
-        <div className="flex flex-col items-center w-screen my-5">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Stat</th>
-                        <th>Value</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.entries(stats).map(([stat, value]) => (
-                        <StatRow
-                            key={stat}
-                            stat={stat}
-                            value={value}
-                            onModify={handleModify}
-                        />
-                    ))}
-                </tbody>
-            </table>
+        <div className="flex justify-center w-screen gap-5 my-5">
+            <div className="w-1/2">
+                <table className="w-full">
+                    <thead>
+                        <tr>
+                            <th>Stat</th>
+                            <th>Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {leftStats.map(([stat, value]) => (
+                            <StatRow
+                                key={stat}
+                                stat={stat}
+                                value={value}
+                                onModify={handleModify}
+                            />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+
+            <div className="w-1/2">
+                <table className="w-full">
+                    <thead>
+                        <tr>
+                            <th>Stat</th>
+                            <th>Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rightStats.map(([stat, value]) => (
+                            <StatRow
+                                key={stat}
+                                stat={stat}
+                                value={value}
+                                onModify={handleModify}
+                            />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
