813f7ea5413226b9d447c4a569577e438cbb18f8
Buddy Stone <buddy.cgn@gmail.com>
2026-03-25 16:10:28 +0100

    refactor chara section

--- src/feature/character/CharacterRouter.tsx
+++ src/feature/character/CharacterRouter.tsx
@@ -1,23 +1,20 @@
-import Preloader from '@/components/Preloader'
-import React, { Suspense } from 'react'
+import React from 'react'
+import CharacterLayout from '../character/layout/CharacterLayout'
 
 const Character = React.lazy(() => import('../character/pages/Character'))
-const ArchetypeDetail = React.lazy(
-    () => import('../character/pages/ArchetypeDetail')
-)
-const MetatypeDetail = React.lazy(
-    () => import('../character/pages/MetatypeDetail')
-)
+const ArchetypeDetail = React.lazy(() => import('../character/pages/ArchetypeDetail'))
+const MetatypeDetail = React.lazy(() => import('../character/pages/MetatypeDetail'))
 const NameDetail = React.lazy(() => import('../character/pages/NameDetail'))
 
 const CharacterRouter = [
   {
     path: '/character',
+    element: <CharacterLayout />,
     children: [
-      { index: true, element: <Suspense fallback={<Preloader />}><Character /></Suspense> },
-      { path: 'archetype/:id', element: <Suspense fallback={<Preloader />}><ArchetypeDetail /></Suspense> },
-      { path: 'metatype/:id', element: <Suspense fallback={<Preloader />}><MetatypeDetail /></Suspense> },
-      { path: 'name/:tag', element: <Suspense fallback={<Preloader />}><NameDetail /></Suspense> },
+      { index: true, element: <Character /> },
+      { path: 'archetype/:id', element: <ArchetypeDetail /> },
+      { path: 'metatype/:id', element: <MetatypeDetail /> },
+      { path: 'name/:tag', element: <NameDetail /> },
     ],
   },
 ]
